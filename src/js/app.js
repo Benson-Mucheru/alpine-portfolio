87902fd26490372bc22c92ec1f8ca51edcbf17dd
Benson-Mucheru <benmk300@gmail.com>
2025-09-16 11:48:27 +0300

    skill section: Imported html logo svg

--- src/js/app.js
+++ src/js/app.js
@@ -32,27 +32,32 @@ Alpine.data("skills_and_tools", () => ({
   skills: [
     {
       name: "HTML",
-      icon: "https://orange-merry-27.tiiny.site/html.svg",
+      icon: `<svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 28L4 3H28L26 28L16 31L6 28Z" fill="#E44D26"></path> <path d="M26 5H16V29.5L24 27L26 5Z" fill="#F16529"></path> <path d="M9.5 17.5L8.5 8H24L23.5 11H11.5L12 14.5H23L22 24L16 26L10 24L9.5 19H12.5L13 21.5L16 22.5L19 21.5L19.5 17.5H9.5Z" fill="white"></path> </g></svg>`,
       alt: "HTML5 Icon",
     },
     {
       name: "Tailwind",
-      icon: "https://orange-merry-27.tiiny.site/tailwind.svg",
+      icon: "/src/img/icons/skills/tailwind.svg",
       alt: "Tailwind Icon",
     },
     {
       name: "Alpine",
-      icon: "https://pink-dianemarie-1.tiiny.site/alpine.svg",
+      icon: "/src/img/icons/skills/alpine.svg",
       alt: "Apline Icon",
     },
     {
+      name: "PHP",
+      icon: "/src/img/icons/skills/php.svg",
+      alt: "PHP Icon",
+    },
+    {
       name: "Livewire",
-      icon: "https://orange-merry-27.tiiny.site/livewire.svg",
+      icon: "/src/img/icons/skills/livewire.svg",
       alt: "Livewire Icon",
     },
     {
       name: "Laravel",
-      icon: "https://orange-merry-27.tiiny.site/laravel.svg",
+      icon: "/src/img/icons/skills/laravel.svg",
       alt: "Laravel Icon",
     },
   ],
@@ -60,27 +65,27 @@ Alpine.data("skills_and_tools", () => ({
   tools: [
     {
       name: "Git",
-      icon: "src/img/icons/tools/git.svg",
+      icon: "/src/img/icons/tools/git.svg",
       alt: "Git Icon",
     },
     {
       name: "Google Analytics",
-      icon: "src/img/icons/tools/google-analytics.svg",
+      icon: "/src/img/icons/tools/google-analytics.svg",
       alt: "Google Analytics Icon",
     },
     {
       name: "Wordpress",
-      icon: "src/img/icons/tools/wordpress.svg",
+      icon: "/src/img/icons/tools/wordpress.svg",
       alt: "Wordpress Icon",
     },
     {
       name: "Klaviyo",
-      icon: "src/img/icons/tools/klaviyoo.svg",
+      icon: "/src/img/icons/tools/klaviyoo.svg",
       alt: "Klaviyo Icon",
     },
     {
       name: "Litmus",
-      icon: "src/img/icons/tools/litmus.svg",
+      icon: "/src/img/icons/tools/litmus.svg",
       alt: "Litmus Icon",
     },
   ],
